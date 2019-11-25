dba14bc2ecfddfc3ce3aa5c870b62822ad230e5d
Raluca Iordan <raiordan@deloittece.com>
2019-11-25 12:07:10 +0200

    Initial Commit

---
+++ ToDo_TaDa_CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo_TaDa_CoreData
+//
+//  Created by Iordan, Raluca on 25/11/2019.
+//  Copyright © 2019 Iordan, Raluca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
